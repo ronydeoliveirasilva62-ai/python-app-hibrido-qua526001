d84764b31682c6296952283eb0554982ded54291
ronydeoliveirasilva62.oigit config user.email ronydeoliveirasilva62@gmail.com <ronydeoliveirasilva62@gmail.com>
2025-10-17 16:51:49 -0300

    Primeiro commit. Aula do dia 17/10/2025.

---
+++ 01_logica_de_programacao/03_tipos_de_dados/main.py
@@ -0,0 +1,11 @@
+#declaracao de variaveis
+nome = "rony de oliveira"
+idade = 53
+altura = 1.75
+programador = True
+
+# exibe os tipos de dados
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
