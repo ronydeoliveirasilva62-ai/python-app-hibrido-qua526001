a632e58ff74c9d23f6315808f2459b252cff02c8
ronydeoliveirasilva62ai <ronydeoliveirasilva62@gmail.com>
2025-11-27 17:15:18 -0300

    27/11/2025

--- 07_app_hibrido/04_flex_fuel/src/main.py
+++ 07_app_hibrido/04_flex_fuel/src/main.py
@@ -2,6 +2,31 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    def calcular_combustivel(e):
+        if not gasolina.value:
+            gasolina.error_text = "valor da gasolina nao pode ficar vazio."
+            page.update()
+        else:
+            gasolina.error_text = ""
+            page.update()
+        if not Etanol.value:
+            Etanol.error_text = "valor da Etanol nao pode ficar vazio."
+            page.update()
+        else:
+            Etanol.error_text = ""
+
+            gasolina.value =float(gasolina.value.replace(",","."))
+            Etanol.value =float(Etanol.value.replace(",","."))
+            resultado = "gasolina" if Etanol.value >=gasolina.value*0.7 else "Etanol"
+            dlg_modal.content.value = resultado
+            gasolina.value = ""
+            Etanol.value = ""
+
+            page.open(dlg_modal)
+
+            page.update()
+        
+        
     page.title = "App Flex Fuel"
     page.scroll = "adaptive"
     page.theme_mode = ft.ThemeMode.LIGHT
@@ -28,8 +53,9 @@ def main(page: ft.Page):
     page.add(
         ft.SafeArea(
             ft.Container(
-               ft.Text("Flex Fuel", size=40, weight="Bold", color="teal"),
-                alignment=ft.alignment.center,
+               ft.Text("Flex Fuel", size=40, weight="Bold", color="yellow"),
+                alignment=ft.alignment.center,bgcolor="blue",
+                padding=100
             ),
             expand=True,
         ),
@@ -39,8 +65,19 @@ def main(page: ft.Page):
                 ft.Container(Etanol, col={"sm":6, "md":4, "xl":2})
                                             
                                             
-            ]
-        )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        ft.Row(
+            [
+                ft.Container(
+                    ft.ElevatedButton("calcular", on_click=calcular_combustivel),
+                    padding=30
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+            )
+            
     )
 
 
